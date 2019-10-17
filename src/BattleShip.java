19ec8e88a550cf59313351077399d09da90fc387
Vicente <rockyrazorsharp@gmail.com>
2019-10-17 18:50:02 +0200

    remainingShots

--- src/BattleShip.java
+++ src/BattleShip.java
@@ -1,79 +1,94 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class BattleShip {
-	//hello
-	
+	// hello
+
 	public static final int SHIP_SYMBOL = 'S';
 	public static final int WATER_SYMBOL = 'O';
 	public static final int SUNK_SHIP_SYMBOL = 'X';
 	public static final int EMPTY_SYMBOL = '.';
-	
+
 	public static final int NUM_SHIPS = 10;
-	public static final int DIMENSION = 8;	
+	public static final int DIMENSION = 8;
 	static char[][] matrix = new char[DIMENSION][DIMENSION];
 	static boolean gameOver;
-		
+	static int sunkShipCounter;
+	public static final int MAX_SHOTS=30;
+	static int remainingShots;
 	public static void main(String[] args) {
-		
+
 		char letter;
 		int number;
 		Scanner input = new Scanner(System.in);
-			     	     	
+
 		gameOver = false;
-	    initMatrix();
-	    addShipsToMatrix();
-	    	     
-	    while(!gameOver) {
-	    	printMatrix(true);
-	    	System.out.println("Enter row (Letter):");
-	    	letter = input.next().toUpperCase().charAt(0);
-	    	System.out.println("Enter column (Number): ");
-	    	number = input.nextInt();
-	    	shoot(letter, number);
-	    }
-	     
+		sunkShipCounter = 0;
+		remainingShots=MAX_SHOTS;
+		initMatrix();
+		addShipsToMatrix();
+
+		while (!gameOver) {
+			printMatrix(true);
+			System.out.println("Enter row (Letter):");
+			letter = input.next().toUpperCase().charAt(0);
+			System.out.println("Enter column (Number): ");
+			number = input.nextInt();
+			shoot(letter, number);
+			checkGameOver();
+
+		}
+
 	}
-	
+
+	private static void checkGameOver() {
+		if (sunkShipCounter >= NUM_SHIPS || remainingShots<=0) {
+			gameOver = true;
+		}
+
+	}
+
 	private static void shoot(char letter, int number) {
-		
+
 		int row = letter - 'A';
 		int col = number - 1;
-		
+		remainingShots--;
+
 		if (matrix[row][col] == SHIP_SYMBOL) {
 			matrix[row][col] = SUNK_SHIP_SYMBOL;
-		}else {
+			sunkShipCounter++;
+		} else {
 			if (matrix[row][col] == EMPTY_SYMBOL) {
-				matrix[row][col]= WATER_SYMBOL;
+				matrix[row][col] = WATER_SYMBOL;
 			}
 		}
-		
+
 	}
 
 	private static void addShipsToMatrix() {
-		
+
 		long shipCounter = 0;
 		int randomRow, randomCol;
-		
+
 		while (shipCounter < NUM_SHIPS) {
 			randomRow = (int) (Math.random() * DIMENSION);
-			randomCol= (int) (Math.random() * DIMENSION);
-	
+			randomCol = (int) (Math.random() * DIMENSION);
+
 			if (matrix[randomRow][randomCol] != SHIP_SYMBOL) {
 				matrix[randomRow][randomCol] = SHIP_SYMBOL;
-				shipCounter ++;
+				shipCounter++;
 			}
 		}
-		
+
 	}
 
 	public static void initMatrix() {
 		for (int row = 0; row < matrix.length; row++) {
 			for (int col = 0; col < matrix[0].length; col++) {
-				matrix[row][col] = EMPTY_SYMBOL;				
+				matrix[row][col] = EMPTY_SYMBOL;
 			}
 		}
 	}
-	
+
 	public static void printMatrix(boolean debug) {
 		printHeader();
 		char c = 'A';
@@ -85,12 +100,11 @@ public class BattleShip {
 					if (debug) {
 						System.out.print(matrix[row][col] + " ");
 					} else {
-						System.out.print(Character.toString(EMPTY_SYMBOL)
-								+ " ");
+						System.out.print(Character.toString(EMPTY_SYMBOL) + " ");
 					}
 				} else {
 					System.out.print(matrix[row][col] + " ");
-				}			
+				}
 			}
 			System.out.println();
 		}
@@ -98,11 +112,11 @@ public class BattleShip {
 
 	private static void printHeader() {
 		System.out.print("  ");
-		for(int i = 1; i <= matrix[0].length; i++ ) {
+		for (int i = 1; i <= matrix[0].length; i++) {
 			System.out.print(i + " ");
 		}
 		System.out.println();
-		
+
 	}
 
 }
